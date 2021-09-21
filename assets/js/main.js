5392eee650bb688785097936a32a3dd62348712d
Linda Martinez <lindanmjw@gmail.com>
2021-09-21 08:29:18 -0400

    adding style.css, main.js and index.html files

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+console.log("am i connected?");
+
+// var imgTag = document.createElement("img");
+
+// imgTag.setAttribute("alt", "img");
+
+// imgTag.setAttribute("src", "assets/images/hello.jpg");
+// imgTag.setAttribute("alt", "test image");
+
+// document.body.appendChild(imgTag);
