da4a8fc0491928caa8a78817b2c551aae38f0827
Linda Martinez <lindanmjw@gmail.com>
2021-09-28 17:49:37 -0400

    adding js for question boxes

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,13 +14,56 @@
 // THEN I can save my initials and set my score on the board.
 
 
-const start_btn = document.querySelector("start_btn button");
-const quiz_deetz = document.querySelector("quiz_deetz button");
-const quit = document.querySelector("quit button");
-const startover = document.querySelector("startover button");
+const start_btn = document.querySelector(".start_btn button");
+const quiz_deetz = document.querySelector(".quiz_deetz");
+const quit = quiz_deetz.querySelector(".buttons .quit");
+const startover = quiz_deetz.querySelector(".buttons .startover");
+const quiz_box = document.querySelector(".quiz_box");
 
-// Start button hit
+// Button click****
 
-start_btn.onclick = ()=>{
-    quiz_deetz.classList.add("");
+// Start button - show deetz
+
+start_btn.onclick = () => {
+    quiz_deetz.classList.add("activeInfo");
+}
+
+// // Leave quiz (quit) button - hide deetz
+quit.onclick = () => {
+    quiz_deetz.classList.remove("activeInfo");
+}
+
+// // Start over button - hide deetz
+startover.onclick = () => {
+    quiz_deetz.classList.remove("activeInfo");
+    //     // Show quiz question box
+    quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+}
+
+let fnl_score = 0;
+
+const next_btn = quiz_box.querySelector(".next_btn")
+
+// Next question button clicked
+
+next_btn.onclick = ()=>{
+    if(fnl_score < questions.length - 1){
+        fnl_score++;
+        showQuestions(fnl_score);
+    }else{
+        console.log("Questions completed");
+    }
+}
+
+function showQuestions(index) {
+    const show_quest = document.querySelector(".show_quest");
+    const answer_options = document.querySelector(".answer_options");
+    let quest_tag = '<span>' + questions[index].question + '</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                    + '<div class="option">'+ questions[index].options[2] +'<span></span></div>';
+
+    show_quest.innerHTML = quest_tag;
+    answer_options.innerHTML = option_tag
 }
